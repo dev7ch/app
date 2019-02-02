7ffea0735c9d2daa2ebb683c31cf8547fab69476
Silvan <silvan@dev7.ch>
2019-02-03 00:15:38 +0100

    adjust reote css loading

--- src/helpers/load-extension.js
+++ src/helpers/load-extension.js
@@ -3,6 +3,13 @@
  * @param  {String}   src Location of the file on the web
  * @return {Promise}      Resolves the extension module, rejects loading error
  */
+let index = 0;
+
+// Style urls collector
+let styles = [];
+
+// let auth = JSON.parse(window.sessionStorage['directus-sdk-js']);
+
 export default function loadExtension(src) {
   return new Promise((resolve, reject) => {
     const script = document.createElement("script");
@@ -25,19 +32,52 @@ export default function loadExtension(src) {
     }
 
     link.rel = "stylesheet";
-
     // NOTE:
     // The src is always a .js file. We can retrieve the extension's CSS by
-    //   by fetching the same path with the css extension
+    // by fetching the same path with the css extension
     link.href = src.slice(0, -2) + "css";
-    link.onerror = function() {
-      this.remove();
-    };
+    link.id = 'api-css-' + src.slice(0, -2).split('/').slice(-2)[0];
 
     script.onload = onload;
     script.onerror = onerror;
     script.src = src;
+
     document.body.appendChild(script);
-    document.body.appendChild(link);
-  });
+
+    // Collection styles (no in use)
+    styles.push(link);
+
+    // Check if if remote css file exists
+    async function fileExists(file) {
+      let xhr = new XMLHttpRequest(),
+        method = "HEAD",
+        url = file;
+      xhr.open(method, url, true)
+      xhr.send(null)
+      xhr.onreadystatechange = function () {
+        // Request finished. Do processing here.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+          // Request finished. Do processing here.
+          document.head.appendChild(link);
+
+          console.log(xhr)
+          console.log('Found', link.href)
+        } else if (this.status === 404) {
+          xhr.abort()
+        }
+      };
+      xhr.onerror = function () {
+        return console.log(this,  'sdlkfjdlkfj')
+      }
+    };
+
+    try {
+      fileExists(link.href);
+
+    } catch (e) {
+      return console.log(e.message)
+    }
+    index++;
+  })
 }
+

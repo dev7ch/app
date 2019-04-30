18a43d5fa36fb9afcadaedfcdd447c96c8e4e6ce
dev7ch <silvan@dev7.ch>
2019-04-30 09:58:54 +0100

    adding sotable plugin to checkboxes

--- src/interfaces/checkboxes/input.vue
+++ src/interfaces/checkboxes/input.vue
@@ -1,31 +1,70 @@
 <template>
-  <div class="interface-checkboxes">
-    <v-checkbox
-      v-for="(label, val) in options.choices"
-      :id="label"
-      :key="label"
-      :value="name + '-' + val"
-      :disabled="readonly"
-      :label="label"
-      :checked="selection.includes(val)"
-      @change="updateValue(val, $event)"
-    ></v-checkbox>
-  </div>
+  <draggable
+    class="interface-checkboxes draggable"
+    element="div"
+    v-model="list"
+    v-bind="dragOptions"
+    @end="saveSort()"
+    @start="getChoices()"
+  >
+    <transition-group v-for="(label, val, idx) in options.choices" :key="idx">
+      <v-checkbox
+        :id="label"
+        :key="label"
+        :value="name + '-' + val"
+        :disabled="readonly"
+        :label="label"
+        :checked="selection.includes(val)"
+        @change="updateValue(val, $event)"
+      ></v-checkbox>
+    </transition-group>
+  </draggable>
 </template>
 
 <script>
 import mixin from "@directus/extension-toolkit/mixins/interface";
+import draggable from "vuedraggable";
 
 export default {
   name: "interface-checkboxes",
   mixins: [mixin],
+  watch: {
+    isDragging(newValue) {
+      if (newValue) {
+        this.delayedDragging = true;
+        return;
+      }
+      this.$nextTick(() => {
+        this.delayedDragging = false;
+      });
+    }
+  },
+
+  components: {
+    draggable
+  },
+
   computed: {
+    dragOptions() {
+      return {
+        animation: 0,
+        group: "description",
+        disabled: !this.editable,
+        ghostClass: "ghost"
+      };
+    },
+    fieldsWithSort() {
+      return this.fields.map((field, index) => ({
+        ...field,
+        sort: index + 1
+      }));
+    },
     selection() {
-      if (this.value == null) return [];
+      if (!this.value) return [];
 
       let selection;
 
-      // Conver the value to an array
+      // Convert the value to an array
       if (typeof this.value === "string") {
         if (this.value.includes(",")) {
           selection = selection.split(",");
@@ -44,7 +83,40 @@ export default {
       return selection;
     }
   },
+
+  data() {
+    return {
+      list: [],
+      editable: true,
+      isDragging: false,
+      delayedDragging: false
+    };
+  },
+
   methods: {
+    saveSort() {
+      // console.log(this.values)
+      // console.log(this.value)
+      console.log(this.list);
+    },
+    getChoices() {
+      let choices = this.value;
+      if (typeof choices === "string") {
+        if (choices.includes(",")) {
+          choices = choices.split(",");
+        } else {
+          choices = [choices];
+        }
+      }
+      console.log(choices);
+      //this.list = choices;
+    },
+    sort() {
+      this.options.map((name, index) => {
+        return { name, order: index + 1, fixed: false };
+      });
+    },
+
     updateValue(val) {
       let selection = [...this.selection];
 

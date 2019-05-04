3f310141f542a9c06f7839cbe26948defb1c21e9
dev7ch <silvan@dev7.ch>
2019-05-04 11:30:26 +0100

    save sortings of seleted arrray

--- src/interfaces/checkboxes/input.vue
+++ src/interfaces/checkboxes/input.vue
@@ -1,14 +1,30 @@
 <template>
   <draggable
     class="interface-checkboxes draggable"
+    :class="{ sortable: sortable }"
     element="div"
-    v-model="list"
+    v-model="sortableList"
     v-bind="dragOptions"
     @end="saveSort()"
-    @start="getChoices()"
+    @start="setSort()"
+    draggable=".sortable-box"
   >
-    <transition-group v-for="(label, val, idx) in options.choices" :key="idx">
+    <template v-if="sortable">
+      <div v-for="(item, idx) in sortableList" :key="idx" class="sortable-box">
+        <v-checkbox
+          :id="item.label"
+          :key="item.label"
+          :value="item.val"
+          :disabled="readonly"
+          :label="item.label"
+          :checked="selection.includes(item.val || idx)"
+          @change="updateValue(item.val, $event)"
+        ></v-checkbox>
+      </div>
+    </template>
+    <template v-else-if="!sortable">
       <v-checkbox
+        v-for="(label, val) in options.choices"
         :id="label"
         :key="label"
         :value="name + '-' + val"
@@ -17,7 +33,7 @@
         :checked="selection.includes(val)"
         @change="updateValue(val, $event)"
       ></v-checkbox>
-    </transition-group>
+    </template>
   </draggable>
 </template>
 
@@ -28,15 +44,10 @@ import draggable from "vuedraggable";
 export default {
   name: "interface-checkboxes",
   mixins: [mixin],
-  watch: {
-    isDragging(newValue) {
-      if (newValue) {
-        this.delayedDragging = true;
-        return;
-      }
-      this.$nextTick(() => {
-        this.delayedDragging = false;
-      });
+  props: {
+    sortable: {
+      type: Boolean,
+      default: true
     }
   },
 
@@ -47,23 +58,15 @@ export default {
   computed: {
     dragOptions() {
       return {
-        animation: 0,
+        animation: 3,
         group: "description",
         disabled: !this.editable,
         ghostClass: "ghost"
       };
     },
-    fieldsWithSort() {
-      return this.fields.map((field, index) => ({
-        ...field,
-        sort: index + 1
-      }));
-    },
     selection() {
       if (!this.value) return [];
-
       let selection;
-
       // Convert the value to an array
       if (typeof this.value === "string") {
         if (this.value.includes(",")) {
@@ -74,52 +77,74 @@ export default {
       } else {
         selection = this.value;
       }
-
       if (this.options.wrap && selection.length > 2) {
         selection.pop();
         selection.shift();
       }
-
       return selection;
+    },
+    choices() {
+      if (!this.$props.options.choices) return [];
+      let choice;
+      // Convert the value to an array
+      if (typeof this.$props.options.choices === "string") {
+        if (this.$props.options.choices.includes(",")) {
+          choice = choice.split(",");
+        } else {
+          choice = [this.$props.options.choices];
+        }
+      } else if (typeof this.$props.options.choices === "object") {
+        choice = this.$props.options.choices;
+        choice = Object.keys(this.$props.options.choices).map(k => ({
+          val: k,
+          label: this.$props.options.choices[k],
+          sort: k.ke
+        }));
+      }
+      console.log(choice);
+      return choice;
     }
   },
 
   data() {
     return {
-      list: [],
+      sortableList: [],
       editable: true,
       isDragging: false,
       delayedDragging: false
     };
   },
 
+  mounted() {
+    if (this.$props.sortable) {
+      this.initSortable();
+    }
+  },
+
   methods: {
     saveSort() {
-      // console.log(this.values)
-      // console.log(this.value)
-      console.log(this.list);
-    },
-    getChoices() {
-      let choices = this.value;
-      if (typeof choices === "string") {
-        if (choices.includes(",")) {
-          choices = choices.split(",");
-        } else {
-          choices = [choices];
-        }
-      }
-      console.log(choices);
-      //this.list = choices;
-    },
-    sort() {
-      this.options.map((name, index) => {
-        return { name, order: index + 1, fixed: false };
+      let selection = [...this.selection];
+      let staged = this.$lodash.map(this.sortableList, function(value) {
+        return value.val;
       });
+      staged = staged.filter(val => selection.includes(val));
+      console.log(staged);
+      console.log(this.options);
+      return this.$emit("input", staged);
+    },
+
+    setSort() {
+      this.initSortable();
+    },
+
+    initSortable() {
+      this.sortableList = [...this.choices];
     },
 
     updateValue(val) {
       let selection = [...this.selection];
 
+      console.log(val);
       if (selection.includes(val)) {
         selection.splice(selection.indexOf(val), 1);
       } else {
@@ -135,6 +160,8 @@ export default {
       if (this.type === "array") {
         selection = selection.split(",");
       }
+
+      console.log(selection);
 
       this.$emit("input", selection);
     }

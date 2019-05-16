aac18f193f8ddce977e7f5afe878c5406b2dd526
dev7ch <silvan@dev7.ch>
2019-05-16 10:34:21 +0100

    adding files

--- src/interfaces/wysiwyg-full/extensions/custom/Highlight.js
+++ src/interfaces/wysiwyg-full/extensions/custom/Highlight.js
@@ -1,14 +1,11 @@
-import {Mark, updateMark, updateAttrs} from "tiptap"
-import HightlightView from "./view/highlight"
-export default class highlight extends Mark
-{
-  get name()
-  {
-    return 'highlight'
+import { Mark, updateMark, updateAttrs } from "tiptap";
+import HightlightView from "./view/highlight";
+export default class highlight extends Mark {
+  get name() {
+    return "highlight";
   }
 
-  get schema()
-  {
+  get schema() {
     return {
       attrs: {
         "highlight-id": {
@@ -18,37 +15,35 @@ export default class highlight extends Mark
       inclusive: true,
       parseDOM: [
         {
-          tag: 'highlight',
+          tag: "highlight",
           getAttrs: dom => ({
-            "highlight-id": dom.getAttribute('highlight-id'),
-          }),
-        },
+            "highlight-id": dom.getAttribute("highlight-id")
+          })
+        }
       ],
-      toDOM: mark => ['highlight', {
-        "highlight-id": mark.attrs["highlight-id"],
-      }, 0],
-    }
+      toDOM: mark => [
+        "highlight",
+        {
+          "highlight-id": mark.attrs["highlight-id"]
+        },
+        0
+      ]
+    };
   }
 
-  commands({ type, schema })
-  {
-    return attrs =>
-    {
+  commands({ type, schema }) {
+    return attrs => {
       let newHighlightId;
-      if(attrs && attrs["highlight-id"])
-      {
+      if (attrs && attrs["highlight-id"]) {
         newHighlightId = attrs["highlight-id"];
+      } else {
+        newHighlightId = parseInt(Math.random() * 10000000);
       }
-      else
-      {
-        newHighlightId = parseInt(Math.random()*10000000);
-      }
-      return updateMark(type, {"highlight-id" : newHighlightId});
-    }
+      return updateMark(type, { "highlight-id": newHighlightId });
+    };
   }
 
-  get view()
-  {
+  get view() {
     return HightlightView;
   }
 }
--- src/interfaces/wysiwyg-full/extensions/custom/views/highlight.vue
+++ src/interfaces/wysiwyg-full/extensions/custom/views/highlight.vue
@@ -1,31 +1,26 @@
 <template>
-    <span class="highlight" @click="openDetails()">
-        <span ref="content"
-              class=""
-              :highlight-id="idhighlight"
-        /></span>
+  <span class="highlight" @click="openDetails()">
+    <span ref="content" class="" :highlight-id="idhighlight" />
+  </span>
 </template>
 <script>
-  export default {
-    name: "HighlightView",
-    props: ['node', 'updateAttrs', 'editable'],
-    data() {
-      return {}
-    },
-    computed:
-      {
-        idhighlight: {
-          get() {
-            return this.node.attrs["highlight-id"];
-          },
-        },
-      },
-    methods:
-      {
-        openDetails: function () {
-//Do Stuff
-        }
+export default {
+  name: "HighlightView",
+  props: ["node", "updateAttrs", "editable"],
+  data() {
+    return {};
+  },
+  computed: {
+    idhighlight: {
+      get() {
+        return this.node.attrs["highlight-id"];
       }
+    }
+  },
+  methods: {
+    openDetails: function() {
+      //Do Stuff
+    }
   }
-
+};
 </script>

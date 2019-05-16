29b0aa7b8b42c3dbd9f83833da0607af0241cd24
dev7ch <silvan@dev7.ch>
2019-05-16 10:33:33 +0100

    adding and example from github

---
+++ src/interfaces/wysiwyg-full/extensions/custom/Highlight.js
@@ -0,0 +1,54 @@
+import {Mark, updateMark, updateAttrs} from "tiptap"
+import HightlightView from "./view/highlight"
+export default class highlight extends Mark
+{
+  get name()
+  {
+    return 'highlight'
+  }
+
+  get schema()
+  {
+    return {
+      attrs: {
+        "highlight-id": {
+          default: null
+        }
+      },
+      inclusive: true,
+      parseDOM: [
+        {
+          tag: 'highlight',
+          getAttrs: dom => ({
+            "highlight-id": dom.getAttribute('highlight-id'),
+          }),
+        },
+      ],
+      toDOM: mark => ['highlight', {
+        "highlight-id": mark.attrs["highlight-id"],
+      }, 0],
+    }
+  }
+
+  commands({ type, schema })
+  {
+    return attrs =>
+    {
+      let newHighlightId;
+      if(attrs && attrs["highlight-id"])
+      {
+        newHighlightId = attrs["highlight-id"];
+      }
+      else
+      {
+        newHighlightId = parseInt(Math.random()*10000000);
+      }
+      return updateMark(type, {"highlight-id" : newHighlightId});
+    }
+  }
+
+  get view()
+  {
+    return HightlightView;
+  }
+}
---
+++ src/interfaces/wysiwyg-full/extensions/custom/views/highlight.vue
@@ -0,0 +1,31 @@
+<template>
+    <span class="highlight" @click="openDetails()">
+        <span ref="content"
+              class=""
+              :highlight-id="idhighlight"
+        /></span>
+</template>
+<script>
+  export default {
+    name: "HighlightView",
+    props: ['node', 'updateAttrs', 'editable'],
+    data() {
+      return {}
+    },
+    computed:
+      {
+        idhighlight: {
+          get() {
+            return this.node.attrs["highlight-id"];
+          },
+        },
+      },
+    methods:
+      {
+        openDetails: function () {
+//Do Stuff
+        }
+      }
+  }
+
+</script>

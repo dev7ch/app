ff8800d8f63a0e9c0da8b281ab9a5a09c5075e50
dev7ch <silvan@dev7.ch>
2019-12-04 15:54:20 +0100

    adjust hard break

--- src/interfaces/wysiwyg-full/input.vue
+++ src/interfaces/wysiwyg-full/input.vue
@@ -273,8 +273,6 @@ export default {
               return new Code();
             case "code_block":
               return new CodeBlock();
-            case "hardbreak":
-              return new HardBreak();
             case "history":
               return new History();
             case "horizontal_rule":
@@ -299,6 +297,7 @@ export default {
               return new Iframe();
             default:
               return [
+                new HardBreak(),
                 new Heading(),
                 new Placeholder({
                   emptyClass: "is-empty",
